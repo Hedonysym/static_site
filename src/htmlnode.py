21d036b12c2fcc1ee3a9a8bca909b1a3f98820eb
Hedonysym <dallinhad123@gmail.com>
2025-04-28 20:28:37 -0500

    leafnode works

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,3 @@
-
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -20,3 +19,18 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode contains: {self.tag}, {self.value}, {self.children}, {self.props}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("LeafNode must have value")
+        elif self.tag == None:
+            return self.value
+        elif self.props == None:
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,32 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
+from htmlnode import *
 
+"""
+class TestHTMLNode(unittest.TestCase):
+    def test_1(self):
+        node = HTMLNode("hi")
+        node2 = HTMLNode("hi")
+        print(node.__repr__())
+        print(node2.__repr__())
+        self.assertEqual(node.__repr__(), node2.__repr__())
+    
+    def test_2(self):
+        node = HTMLNode("guacamole", "nigga", None, {"pe": "nis"})
+        node2 = HTMLNode("fuck", "shit", None, {"ass": "bitch"})
+        print(node.__repr__())
+        print(node2.__repr__())
+        self.assertNotEqual(node, node2)
+
+    def test_3(self):
+        node = HTMLNode("a", "b", ["c"], {"d": "e", "f": "g"})
+        expected = " d=\"e\" f=\"g\""
+        print(node.__repr__())
+        print(node.props_to_html())
+        self.assertEqual(node.props_to_html(), expected)
+"""
+"""
 class TestTextNode(unittest.TestCase):
     def test_1(self):
         node = TextNode("This is a text node", TextType.BOLD)
@@ -17,6 +42,24 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.NORMAL)
         node2 = TextNode("This is a text node", TextType.NORMAL, "cock.coma")
         self.assertNotEqual(node, node2)
+"""
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test2(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), f'<a href="https://www.google.com">Click me!</a>')
+
+    def test3(self):
+        node = LeafNode(None, "Guacamole nigga penis. :)")
+        print(node.to_html())
+        self.assertTrue(node.to_html(), "Guacamole nigga penis. :)")
+
+    def test4(self):
+        node = LeafNode("i", None)
+        self.assertRaises(ValueError)
                
 
 if __name__ == "__main__":
