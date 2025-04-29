10470aabefda9a6ec64b85f0f63afdfc4e27a43d
Hedonysym <dallinhad123@gmail.com>
2025-04-29 15:52:23 -0500

    image and url tuple grabber works

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,6 @@
 from htmlnode import *
 from textnode import *
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -14,3 +15,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 filter(None, split), 
                 [TextType.TEXT, text_type, TextType.TEXT]))
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((http.*?)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"[^!]\[(.*?)\]\((http.*?)\)", text)
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,28 +2,20 @@ import unittest
 
 from textnode import *
 from htmlnode import *
-from parser import split_nodes_delimiter
+from parser import *
 
 
-class TestTextNodeDelimiter(unittest.TestCase):
+class TestTextNodeimageandurlgrab(unittest.TestCase):
     def test_1(self):
-        node = TextNode("This is text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        print(new_nodes)
-        self.assertEqual(new_nodes, [
-    TextNode("This is text with a ", TextType.TEXT, None),
-    TextNode("code block", TextType.CODE, None),
-    TextNode(" word", TextType.TEXT, None),
-    ])
-
-    def test2(self):
-        node = TextNode("This is text with a **bold** word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
-        print(new_nodes)
-        self.assertEqual(new_nodes, [
-    TextNode("This is text with a ", TextType.TEXT, None),
-    TextNode("bold", TextType.BOLD, None),
-    TextNode(" word", TextType.TEXT, None),])
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        print(extract_markdown_images(text))
+        self.assertEqual(extract_markdown_images(text), [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+  
+    def test_2(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        print(extract_markdown_links(text))
+        self.assertEqual(extract_markdown_links(text), [("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+  
 
 
 if __name__ == "__main__":
