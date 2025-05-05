79e1dc6e46ae3fe2f7941c4205d7e853b6f1db9c
Hedonysym <dallinhad123@gmail.com>
2025-05-04 22:35:50 -0500

    pages pushed to github

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import markdown_blocks
 import copy_static
 import generate_pages
+import sys
 
 def main():
-   copy_static.public_to_static()
+   basepath = sys.argv if len(sys.argv) > 1 else "/"
+   
    contentpath = generate_pages.extend_path_to_root("content")
    templatepath = generate_pages.extend_path_to_root("template.html")
-   publicpath = generate_pages.extend_path_to_root("public")
-   generate_pages.generate_pages_recursive(contentpath, templatepath, publicpath)
+   publicpath = generate_pages.extend_path_to_root("docs")
+   copy_static.public_to_static(publicpath)
+   generate_pages.generate_pages_recursive(contentpath, templatepath, publicpath, basepath)
 
 
 
